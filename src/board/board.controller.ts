7fc1c16fdd000ea1aa209e28d9f3347bc8a47b38
Jeon-HS4 <jin0004@tukorea.ac.kr>
2023-07-03 01:26:44 +0900

    feat: ch03 CRUD 기능 추가

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { Board } from './board.model';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -18,5 +18,23 @@ export class BoardController {
         @Body() createBoardDto: CreateBoardDto
     ): Board {
         return this.boardService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: String): Board{
+
+        return this.boardService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: String): void{
+        this.boardService.deleteBoard(id);
+    }
+
+    @Patch('/:id')
+    updateBoard(
+        @Param('id') id: String,
+        @Body('status') status: BoardStatus): Board{
+        return this.boardService.updateBoardStatus(id, status);
     }
 }
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
-import {v1 as uuid} from 'uuid';
+import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
@@ -21,6 +21,20 @@ export class BoardService {
         }
 
         this.board.push(board);
+        return board;
+    }
+
+    getBoardById(id: String): Board{
+        return this.board.find((board) => board.id === id);
+    }
+
+    deleteBoard(id: String): void{
+        this.board = this.board.filter((board) => board.id !== id);
+    }
+
+    updateBoardStatus(id: String, status: BoardStatus): Board{
+        const board =  this.getBoardById(id);
+        board.status = status;
         return board;
     }
 }
